35fd11cdb089493f60d7e0bee43c319877894f96
Marvin Heinrich Esterhuizen <mesterhu@amazon.com>
2023-11-14 11:53:49 +0200

    Support for Js,Ts, Rust, Toml, Lua

--- plugins/mason.lua
+++ plugins/mason.lua
@@ -7,7 +7,10 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "lua_ls",
+        "lua_ls",
+        "tsserver",
+        "rust_analyzer",
+        "typst_lsp",
       })
     end,
   },
@@ -18,8 +21,9 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-        -- "prettier",
-        -- "stylua",
+        "eslint",
+        "prettier",
+        "stylua",
       })
     end,
   },
